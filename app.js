b74d71d221fbd0caca1ff2b3674b3c0a67b157f8
Pratik Sah <shivam3448@gmail.com>
2020-11-04 19:46:04 +0530

    Backend part done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -12,3 +13,16 @@ app.use(bodyParser.json());
 
 // mongodb uri
 const db = process.env.MONGO_URI;
+
+// connect to mongoDb
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log(err));
+
+// use routes
+app.use("/api/items/", items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Listening on port 5000"));
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <span className="logo">Shopping List</span>
+    </nav>
+  );
+}
+
+export default Navbar;
